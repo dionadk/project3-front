73855c439b530c952899604adb4ca52ca9b9c553
Aaliyah Melton <aaliyahmariah09@gmail.com>
2017-10-06 10:15:04 -0400

    Basic Functionality For Comment

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
 export default class Comments extends Component {
-
-
-
+  constructor(props)
+  super(props)
+  this.state = {
+    comments: this.props.comments
+    this.state = {
+      name: '',
+      content: '',
+      date: ''
+    }
+  }
   render () {
+    <div className="comment">
+      <div className="actor">
+        {this.props.name}
+    </div>
+    <div className="content">
+       {this.props.content}
+    </div>
+    <div className="date">
+       {this.props.date}
+    </div>
     return ()
   }
 
