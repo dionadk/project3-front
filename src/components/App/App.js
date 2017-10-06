c06706e33b5da7712b90dbe3a2bee97682b53267
Diona Kurien <diona.david@gmail.com>
2017-10-06 10:13:26 -0400

    Posts are up in front-end

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react';
-import Posts from '../Posts/Posts.js';
-import Home from '../Comments/Comments.js';
+import Post from '../Post/Post.js';
+import Comments from '../Comments/Comments.js';
 
-import './App.css';
+// import './App.css';
+import axios from "axios";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch
+} from "react-router-dom"
 
 
 class App extends Component {
+  constructor(props) {
+    super (props)
+    this.state = {
+      posts: [
+    {"userName": "Diona", "title": "More on React", "date": "10/05/2017", "content": "The more practice the better you will be. Try youtube tutorials at the begenning to get a deep knowledge"},
+    {"userName": "Aalliyah", "title": "DEsign Ideas", "date": "09/05/2017", "content": "The more practice the better you will be. Try youtube tutorials at the begenning to get a deep knowledge"},
+    {"userName": "Max", "title": "Html and css", "date": "08/05/2017", "content": "The more practice the better you will be. Try youtube tutorials at the begenning to get a deep knowledge"},
+    {"userName": "Drew", "title": "Redux", "date": "10/01/2017", "content": "The more practice the better you will be. Try youtube tutorials at the begenning to get a deep knowledge"},
+    {"userName": "Netsue", "title": "More on JS", "date": "08/05/2017", "content": "The more practice the better you will be. Try youtube tutorials at the begenning to get a deep knowledge"},
+    {"userName": "Melley", "title": "More on React", "date": "07/05/2017", "content": "The more practice the better you will be. Try youtube tutorials at the begenning to get a deep knowledge"}
+    ]}
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
 
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <Router>
+          <div>
+            <div className="nav">
+              <div className="nav-item"><span className="nav-logo">GA Posts</span></div>
+              <div className="nav-item"><Link to="/">Posts</Link></div>
 
-      </div>
+            </div>
+            <div className="main">
+              <Switch>
+                <Route exact path="/" render={ () => (
+                <Post posts={this.state.posts} />
+                )}
+                />
+
+                <Route
+                  path="/*"
+                  render={ () => (<Redirect to="/" />)}
+                />
+              </Switch>
+            </div>
+          </div>
+        </Router>
     );
   }
 }
