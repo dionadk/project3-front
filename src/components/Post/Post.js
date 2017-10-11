9b7bcfa8040fb51ec02a864878ab2659dd570b09
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-10-11 11:09:00 -0400

    working feature

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -8,23 +8,42 @@ export default class Post extends Component {
   constructor(props) {
   super(props)
   this.state = {
-    posts: this.props.posts
+    // posts: this.props.posts
   }
 
 }
 
-  render () {
+
+render () {
+
+  // sorts posts newest to oldest
+  let posts = []
+  this.props.posts.forEach(post => {posts.push(post)})
+
+  // compares posts for .sort
+  function compare(a, b) {
+    if (a.createdAt > b.createdAt)
+      return -1;
+    if (a.createdAt < b.createdAt)
+      return 1;
+    return 0;
+  }
+  // new variable for sorted posts
+  let sortedPosts = posts.sort(compare)
+
+
+
     return (
         <div>
           <h2>Posts</h2>
           <div className='row'>
-            {this.props.posts.map( post => {
+          {sortedPosts.map( post => {
               return (
                 <Link to={`/${post._id}`}>
                   <div className="blogPost col s12" key={post._id}>
                     <h3>{post.title}</h3>
+                    <p> {post.createdAt} </p>
                     <h6 className='col s12'>
-                      {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. */}
                       {post.content}
                     </h6>
                   </div>
